95f3a86e2a9d976101b94fe5a55a8e5dc355c80f
Gabriel Smith <gabrielsmith@Gabriels-MacBook-Pro-2.local>
2021-11-16 00:30:45 -0600

    small changes

--- src/views/Countdown.js
+++ src/views/Countdown.js
@@ -85,7 +85,7 @@ export default function Countdown() {
             </section>
           </div>
           <div>
-            <Link className="nav-link tickets" to="/tickets">
+            <Link className="nav-link tickets" to="/schedule">
               Get Tickets To A Game!
             </Link>
           </div>
