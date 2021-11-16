d58daa6549d84b5f80e2f8c0a5434bb3f4c88f1d
Gabriel Smith <gabrielsmith@Gabriels-MacBook-Pro-2.local>
2021-11-15 23:43:27 -0600

    spring training and opening  day count down

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,1 +1,19 @@
-// index for router
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Countdown from '../views/Countdown';
+import OpeningDay from '../views/OpeningDay';
+
+export default function Routes() {
+  return (
+    <>
+      <Switch>
+        <Route exact path="/">
+          <Countdown />
+        </Route>
+        <Route exact path="/openingday">
+          <OpeningDay />
+        </Route>
+      </Switch>
+    </>
+  );
+}
---
+++ src/views/OpeningDay.js
@@ -0,0 +1,88 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+export default function OpeningDay() {
+  const [timerDays, setTimerDays] = useState('00');
+  const [timerHours, setTimerHours] = useState('00');
+  const [timerMinutes, setTimerMinutes] = useState('00');
+  const [timerSeconds, setTimerSeconds] = useState('00');
+
+  let interval = useRef();
+
+  const startTimer = () => {
+    const countdownDate = new Date('March 31 2022 00:00:00').getTime();
+
+    interval = setInterval(() => {
+      const now = new Date().getTime();
+      const distance = countdownDate - now;
+
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+      );
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      if (distance < 0) {
+        clearInterval(interval.current);
+      } else {
+        setTimerDays(days);
+        setTimerHours(hours);
+        setTimerMinutes(minutes);
+        setTimerSeconds(seconds);
+      }
+    }, 1000);
+  };
+
+  useEffect(() => {
+    startTimer();
+    return () => {
+      clearInterval(interval.current);
+    };
+  });
+
+  return (
+    <>
+      <section className="time-container">
+        <section className="timer">
+          <div className="countdown-header">
+            <h2>Countdown To Opening Day</h2>
+            <Link className="nav-link active" to="/">
+              Spring Training
+            </Link>
+          </div>
+          <br />
+          <div className="count-down-timer">
+            <section>
+              <p>{timerDays}</p>
+              <p>
+                <small>Days</small>
+              </p>
+            </section>
+            <span>:</span>
+            <section>
+              <p>{timerHours}</p>
+              <p>
+                <small>Hours</small>
+              </p>
+            </section>
+            <span>:</span>
+            <section>
+              <p>{timerMinutes}</p>
+              <p>
+                <small>Minutes</small>
+              </p>
+            </section>
+            <span>:</span>
+            <section>
+              <p>{timerSeconds}</p>
+              <p>
+                <small>Seconds</small>
+              </p>
+            </section>
+          </div>
+        </section>
+      </section>
+    </>
+  );
+}
